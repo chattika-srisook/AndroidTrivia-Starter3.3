56ab3831ce72dd84b42d43f7c3040f253613e292
your@email.com <you@example.com>
2020-07-27 15:51:35 +0700

    3.2Step 2: Add a click handler to the Play button

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,23 +1,18 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TitleFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -36,11 +31,13 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
                 R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener{view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)}
         return binding.root
     }
 
     companion object {
-        
+
         fun newInstance(param1: String, param2: String) =
                 TitleFragment().apply {
                     arguments = Bundle().apply {
